7a2ff0c75a11982ff0ea1f76f6907b883ecdfd32
jarode <jarode2@o2.pl>
2012-09-18 14:30:01 +0100

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_locale
+ 
+
   protect_from_forgery
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
+  
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end  
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @users = User.all
 	@auctions = Auction.all
+	@tags = Tag.all
   end
 end
